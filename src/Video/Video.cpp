b741466646e4415345f631d9f7be66b6f04cc121
Miguel Almeida <miguel.moitinho@ist.utl.pt>
2013-11-22 12:33:16 +0000

    Video opener and frame grabber

--- src/Video/Video.cpp
+++ src/Video/Video.cpp
@@ -7,6 +7,33 @@ Video::Video(){
 	return;
 }
 
-Video::Video(cv::VideoCapture* capture){
+Video::Video(cv::VideoCapture capture){
 	cap = capture;
+	check_cap();
+}
+
+Video::Video(std::string filename){
+	cap = cv::VideoCapture(filename);
+	check_cap();
+}
+
+/********************************************************************************
+ * Capture functions
+ *******************************************************************************/
+bool Video::check_cap(){
+	if(cap.isOpened()){
+		return true;
+	}
+	std::cerr << "Couldn't open " << filename << std::endl;
+	return false;
+}
+
+bool Video::get_frame(cv::Mat &frame){
+	if(cap.read(frame)){
+		return true;
+	}
+	else{
+		std::cerr << "Couldn't read frame" << std::endl;
+		return false;
+	}
 }
