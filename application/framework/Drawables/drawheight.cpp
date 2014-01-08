872084d6ded9c8b1691c2c803f1eb017eddeb3b1
Miguel Almeida <miguel.moitinho@ist.utl.pt>
2014-01-08 19:19:58 +0000

    Apply antialiasing to the lines and points.

--- application/framework/Drawables/drawheight.cpp
+++ application/framework/Drawables/drawheight.cpp
@@ -23,15 +23,15 @@ bool DrawHeight::release(cv::Point pi2){
 void DrawHeight::draw(cv::Point pi2, cv::Mat& frame){
     switch (npoints) {
     case 3:
-        cv::circle(frame, points[3], 3, cv::Scalar( 0, 0, 255), -1, 8, 0);
-        cv::line(frame, points[3], points[2], cv::Scalar( 0, 255, 0));
+        cv::circle(frame, points[3], 3, cv::Scalar( 0, 0, 255), -1, CV_AA, 0);
+        cv::line(frame, points[3], points[2], cv::Scalar( 0, 255, 0), 1, CV_AA);
     case 2:
-        cv::circle(frame, points[2], 3, cv::Scalar( 0, 0, 255), -1, 8, 0);
+        cv::circle(frame, points[2], 3, cv::Scalar( 0, 0, 255), -1, CV_AA, 0);
     case 1:
-        cv::circle(frame, points[1], 3, cv::Scalar( 0, 0, 255), -1, 8, 0);
-        cv::line(frame, points[1], points[0], cv::Scalar( 0, 255, 0));
+        cv::circle(frame, points[1], 3, cv::Scalar( 0, 0, 255), -1, CV_AA, 0);
+        cv::line(frame, points[1], points[0], cv::Scalar( 255, 0, 0), 1, CV_AA);
     case 0:
-        cv::circle(frame, points[0], 3, cv::Scalar( 0, 0, 255), -1, 8, 0);
+        cv::circle(frame, points[0], 3, cv::Scalar( 0, 0, 255), -1, CV_AA, 0);
     default:
         break;
     }
