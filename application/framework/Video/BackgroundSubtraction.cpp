fd1ec4e7a2daec607ece126697c946dde3c1fd0d
Miguel Almeida <miguel.moitinho@ist.utl.pt>
2014-02-18 11:21:01 +0000

    Replace Tabs with spaces...

--- application/framework/Video/BackgroundSubtraction.cpp
+++ application/framework/Video/BackgroundSubtraction.cpp
@@ -4,62 +4,62 @@
  * Constructors
  ******************************************************************************/
 BackgroundSubtractor::BackgroundSubtractor(){
-	history = 200;
-	varThreshold = 32;
-	bShadowDetection = true;
-	bgsub = cv::BackgroundSubtractorMOG2(history,  varThreshold,
-		bShadowDetection );
+    history = 200;
+    varThreshold = 32;
+    bShadowDetection = true;
+    bgsub = cv::BackgroundSubtractorMOG2(history,  varThreshold,
+        bShadowDetection );
 }
 
 BackgroundSubtractor::BackgroundSubtractor(int hist, int varThresh,
-		bool bShadowDet){
-	history = hist;
-	varThreshold = varThresh;
-	bShadowDetection = bShadowDet;
-	bgsub = cv::BackgroundSubtractorMOG2(history,
-		varThreshold,
-		bShadowDetection );
+        bool bShadowDet){
+    history = hist;
+    varThreshold = varThresh;
+    bShadowDetection = bShadowDet;
+    bgsub = cv::BackgroundSubtractorMOG2(history,
+        varThreshold,
+        bShadowDetection );
 }
 
 /*******************************************************************************
  * Get foreground, background and contours functions
  ******************************************************************************/
 void BackgroundSubtractor::NewFrame(cv::Mat img, bool f/*=true*/,
-									bool b/*=true*/, bool c/*=false*/){
-	// copy image to object
-	frame = img;
-	// calculate foreground
-	if(f)
-		bgsub(img,fore);
-	// calculate background
-	if(b)
-		bgsub.getBackgroundImage(back);
+                                    bool b/*=true*/, bool c/*=false*/){
+    // copy image to object
+    frame = img;
+    // calculate foreground
+    if(f)
+        bgsub(img,fore);
+    // calculate background
+    if(b)
+        bgsub.getBackgroundImage(back);
     // draw contours
-	if(c){
-		cont = frame.clone();
-		cv::Mat aux = fore.clone();
-		
-		cv::erode(aux,aux,cv::Mat());
-		cv::dilate(aux,aux,cv::Mat());
-		cv::findContours(aux,contours,
-			CV_RETR_EXTERNAL,
-			CV_CHAIN_APPROX_NONE);
-		cv::drawContours(cont,
-			contours,
-			-1,
-			cv::Scalar(0,0,255),
-			1);
-	}
+    if(c){
+        cont = frame.clone();
+        cv::Mat aux = fore.clone();
+
+        cv::erode(aux,aux,cv::Mat());
+        cv::dilate(aux,aux,cv::Mat());
+        cv::findContours(aux,contours,
+            CV_RETR_EXTERNAL,
+            CV_CHAIN_APPROX_NONE);
+        cv::drawContours(cont,
+            contours,
+            -1,
+            cv::Scalar(0,0,255),
+            1);
+    }
 }
 
 cv::Mat BackgroundSubtractor::Foreground(){
-	return fore;
+    return fore;
 }
 
 cv::Mat BackgroundSubtractor::Background(){
-	return back;
+    return back;
 }
 
 cv::Mat BackgroundSubtractor::Contours(){
-	return cont;
+    return cont;
 }
