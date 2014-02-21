3bf8b225c79e8001608f0683d5f0a8c598f10cbd
Miguel Almeida <miguel.moitinho@ist.utl.pt>
2014-02-21 17:07:49 +0000

    Save and load settings working.

--- application/src/QdialogSettings.cpp
+++ application/src/QdialogSettings.cpp
@@ -58,3 +58,47 @@ QString QdialogSettings::getHaarDir(){
 QString QdialogSettings::getCacheDir(){
     return ui->cacheDir->text();
 }
+
+bool QdialogSettings::getUseGPU(){
+    return ui->useGPU->isChecked();
+}
+
+void QdialogSettings::saveSettings(){
+    QFileDialog getDirDialog(this);
+    getDirDialog.setDirectory(QDir::homePath());
+    getDirDialog.setAcceptMode(QFileDialog::AcceptSave);
+
+    QStringList fileNames;
+
+    if(getDirDialog.exec()) {
+        fileNames = getDirDialog.selectedFiles();
+        foreach (QString fileName, fileNames) {
+            QSettings setSave(fileName, QSettings::IniFormat);
+            setSave.setValue("CacheDir",getCacheDir());
+            setSave.setValue("HaarDir",getHaarDir());
+            setSave.setValue("UseGPU",getUseGPU());
+        }
+    }
+}
+
+void QdialogSettings::loadSettings(){
+    QFileDialog getDirDialog(this);
+    getDirDialog.setDirectory(QDir::homePath());
+    getDirDialog.setAcceptMode(QFileDialog::AcceptOpen);
+
+    QStringList fileNames;
+
+    if(getDirDialog.exec()) {
+        fileNames = getDirDialog.selectedFiles();
+        foreach (QString fileName, fileNames) {
+            QSettings setSave(fileName, QSettings::IniFormat);
+
+            ui->cacheDir->setText(
+                        setSave.value("CacheDir", "Uninitialized").toString());
+            ui->haarDir->setText(
+                        setSave.value("HaarDir", "Uninitialized").toString());
+            ui->useGPU->setChecked(
+                        setSave.value("UseGPU", false).toBool());
+        }
+    }
+}
